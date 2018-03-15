11d9a1e34661f97b8d0a3161bce54a60a98e3ae8
Sharena <sdbaron08@gmail.com>
2018-03-15 11:45:02 -0400

    Lesson 21 Security Adding User Registration

--- src/main/java/com/example/demo/UserRepository.java
+++ src/main/java/com/example/demo/UserRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String username);
+    Long countByEmail(String email);
+    Long countByUsername(String username);
 }
